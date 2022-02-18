58bebcba206c0b9b5d6b8ea01f38cb0789679ed8
James <51536154+tsunyoku@users.noreply.github.com>
2022-02-18 23:13:32 +0000

    adjust numbers

--- src/osu/pp.rs
+++ src/osu/pp.rs
@@ -412,7 +412,7 @@ impl OsuPPInner {
         // AR bonus
         let mut ar_factor: f64 = 0.0;
         let required_factor = if self.mods.rx() { 10.5 } else { 10.33 };
-        let buff_factor = if self.mods.rx() { 0.35 } else { 0.3 };
+        let buff_factor = if self.mods.rx() { 0.4 } else { 0.3 };
         if attributes.ar > required_factor {
             ar_factor = buff_factor * (attributes.ar - required_factor)
         } else if attributes.ar < 8.0 {
@@ -484,8 +484,9 @@ impl OsuPPInner {
         // AR bonus
         let mut ar_factor: f64 = 0.0;
         let required_factor = if self.mods.rx() { 10.5 } else { 10.33 };
+        let buff_factor = if self.mods.rx() { 0.4 } else { 0.3 };
         if attributes.ar > required_factor {
-            ar_factor = 0.3 * (attributes.ar - required_factor)
+            ar_factor = buff_factor * (attributes.ar - required_factor)
         } else if attributes.ar < 8.0 {
             ar_factor = 0.1 * (8.0 - attributes.ar)
         }
