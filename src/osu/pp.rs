14264cffcf8e69685e22f2949a2bbf3f85301148
James <51536154+tsunyoku@users.noreply.github.com>
2022-02-19 00:10:05 +0000

    run rust fmt

--- src/osu/pp.rs
+++ src/osu/pp.rs
@@ -397,12 +397,12 @@ impl OsuPPInner {
         let mut aim_value = (5.0 * (raw_aim / 0.0675).max(1.0) - 4.0).powi(3) / 100_000.0;
 
         // Longer maps are worth more
-        let len_bonus = if self.mods.rx() { 
-            0.97 + 0.1 * f64::min(1.0, total_hits / 2000.0) + calculate_length_bonus(total_hits, attributes.aim_difficult_strain_count)
+        let len_bonus = if self.mods.rx() {
+            0.97 + 0.1 * f64::min(1.0, total_hits / 2000.0)
+                + calculate_length_bonus(total_hits, attributes.aim_difficult_strain_count)
         } else {
-            0.95
-            + 0.4 * (total_hits / 2000.0).min(1.0)
-            + (total_hits > 2000.0) as u8 as f64 * 0.5 * (total_hits / 2000.0).log10()
+            0.95 + 0.4 * (total_hits / 2000.0).min(1.0)
+                + (total_hits > 2000.0) as u8 as f64 * 0.5 * (total_hits / 2000.0).log10()
         };
 
         aim_value *= len_bonus;
@@ -474,12 +474,12 @@ impl OsuPPInner {
             (5.0 * (attributes.speed_strain / 0.0675).max(1.0) - 4.0).powi(3) / 100_000.0;
 
         // Longer maps are worth more
-        let len_bonus = if self.mods.rx() { 
-            0.97 + 0.1 * f64::min(1.0, total_hits / 2000.0) + calculate_length_bonus(total_hits, attributes.speed_difficult_strain_count)
+        let len_bonus = if self.mods.rx() {
+            0.97 + 0.1 * f64::min(1.0, total_hits / 2000.0)
+                + calculate_length_bonus(total_hits, attributes.speed_difficult_strain_count)
         } else {
-            0.95
-            + 0.4 * (total_hits / 2000.0).min(1.0)
-            + (total_hits > 2000.0) as u8 as f64 * 0.5 * (total_hits / 2000.0).log10()
+            0.95 + 0.4 * (total_hits / 2000.0).min(1.0)
+                + (total_hits > 2000.0) as u8 as f64 * 0.5 * (total_hits / 2000.0).log10()
         };
 
         speed_value *= len_bonus;
