d96e8907aed67216f07370454846bbba6142078d
James <51536154+tsunyoku@users.noreply.github.com>
2022-02-19 13:58:34 +0000

    oops

--- src/osu/pp.rs
+++ src/osu/pp.rs
@@ -350,8 +350,8 @@ impl OsuPPInner {
             multiplier *= 1.0 - (n_spinners as f64 / self.total_hits).powf(0.85);
         }
 
-        let mut aim_value = self.compute_aim_value();
-        let speed_value = self.compute_speed_value();
+        let aim_value = self.compute_aim_value();
+        let mut speed_value = self.compute_speed_value();
         let acc_value = self.compute_accuracy_value();
         let flashlight_value = self.compute_flashlight_value();
 
