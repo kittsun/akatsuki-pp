ae134ff8669e7f1ad7f96d135fe50c999ead316c
atlas <68862510+kittsun@users.noreply.github.com>
2022-02-20 15:16:57 +0000

    Update pp.rs

--- src/osu/pp.rs
+++ src/osu/pp.rs
@@ -362,9 +362,9 @@ impl OsuPPInner {
 
             let aim_strain_value = a_strain_factor / 10000.0;
             let speed_strain_value = s_strain_factor / 10000.0;
- 
+
             aim_value *= 1.01 + aim_strain_value;
-            speed_value *= 1.01 + speed_strain_value; 
+            speed_value *= 1.01 + speed_strain_value;
         }
 
         if self.mods.rx() {
